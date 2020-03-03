e2a62a5de53641290ae1c631e267b940afcb3e0a
ququlala <alikai812@qq.com>
2020-03-03 20:03:19 +0800

    add elpy

--- init.el
+++ init.el
@@ -46,10 +46,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" default)))
+    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" default)))
  '(package-selected-packages
    (quote
-    (dracula-theme counsel-projectile yasnippet avy counsel ivy subr-x window-numbering company-rtags flycheck-rtags moe-theme nyan-mode solarized-theme org-mode projectile cmake-mode irony company-irony flycheck-irony irony-eldoc use-package undo-tree counsel-projectile company anzu req-package flycheck org-page w3m mime-w3m mime-view dired))))
+    (dracula-theme counsel-projectile yasnippet avy counsel ivy subr-x window-numbering company-rtags flycheck-rtags moe-theme nyan-mode solarized-theme org-mode projectile cmake-mode irony company-irony flycheck-irony irony-eldoc use-package undo-tree counsel-projectile company anzu req-package flycheck org-page w3m mime-w3m mime-view dired ecb2 material-theme elpy))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -397,7 +397,16 @@
 ;;           )
 ;;          ("blog" :components ("blog-notes" "blog-static"))
 ;;          ))
+;; =============================
+;; python setup
+;; =============================
+;; enable elpy
 
+(req-package elpy
+  (progn
+    (elpy-enable
+     ))
+  )
 ;;------------------------------[blog]org-page---------------------
 
 (require 'org-page)
